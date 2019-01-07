434afe65e461c13f523d9c97cf5ddc9f39af0f99
ext_henryc <henry_chou@trendmicro.com>
2019-01-07 09:39:42 +0800

    Kata : Args - 1st

---
+++ Kata.Tests/ArgsTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class ArgsTests
+    {
+        private Args args;
+
+        [SetUp]
+        public void SetUp()
+        {
+            args = new Args();
+            Assert.NotNull(args);
+        }
+
+        [Test]
+        public void GivenBoolFlag_WhenParse_ThenReturnTrue()
+        {
+            string[] result = args.Parse(new string[] { "-l" });
+            Assert.AreEqual(new string[] { Convert.ToString(true) }, result);
+        }
+
+        [Test]
+        public void GivenFlagIntArgument_WhenParse_ThenReturnInteger()
+        {
+            string[] result = args.Parse(new string[] { "-p", "12314" });
+            Assert.AreEqual(new string[] { "12314" }, result);
+        }
+
+        [Test]
+        public void GivenFlagButNotGiveArgument_WhenParse_ThenReturnDefault()
+        {
+            string[] result = args.Parse(new string[] { "-p" });
+            Assert.AreEqual(new string[] { "8080" }, result);
+        }
+
+        [Test]
+        public void GivenTwoFlagWithArgument_WhenParse_ThenReturnValues()
+        {
+            string[] result = args.Parse(new string[] { "-p", "8080", "-l" });
+            Assert.AreEqual(new string[] { "8080", Convert.ToString(true) }, result);
+        }
+
+        [Test]
+        public void GivenThreeFlagWithArgument_WhenParse_ThenReturnValues()
+        {
+            string[] result = args.Parse(new string[] { "-p", "8080", "-l", "-d", "/var/usr/logs" });
+            Assert.AreEqual(new string[] { "8080", Convert.ToString(true), "/var/usr/logs" }, result);
+        }
+
+    }
+}
---
+++ Kata/Args.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata
+{
+    // http://codingdojo.org/kata/Args/
+    // Solution : https://github.com/unclebob/rubyargs/tree/master
+    public class Args
+    {
+        private IList<string> _results;
+        private string[] _args;
+
+        public Args()
+        {
+            _results = new List<string>();
+        }
+
+        public string[] Parse(string[] args)
+        {
+            _args = args;
+            for (int argumentsIndex = 0; argumentsIndex < _args.Length;)
+            {
+                ParseFlag(argumentsIndex);
+                argumentsIndex++;
+            }
+            return _results.ToArray();
+        }
+
+        private void ParseFlag(int argumentsIndex)
+        {
+            bool isFlag = _args[argumentsIndex].ElementAt(0) == '-';
+            if (isFlag)
+            {
+                char flag = _args[argumentsIndex].ElementAt(1);
+                switch (flag)
+                {
+                    case 'p':
+                        ParseFlagWithValue(argumentsIndex, "8080");
+                        break;
+                    case 'l':
+                        _results.Add(Convert.ToString(true));
+                        break;
+                    case 'd':
+                        ParseFlagWithValue(argumentsIndex, "/var/logs");
+                        break;
+                    default:
+                        _results.Add("Not Support Flag");
+                        break;
+                }
+            }
+        }
+
+        private void ParseFlagWithValue(int argumentsIndex, string defaultValue)
+        {
+            if (argumentsIndex < _args.Length - 1 && _args[argumentsIndex + 1][0] != '-')
+            {
+                _results.Add(_args[argumentsIndex + 1]);
+            }
+            else
+            {
+                _results.Add(defaultValue);
+            }
+        }
+    }
+}
