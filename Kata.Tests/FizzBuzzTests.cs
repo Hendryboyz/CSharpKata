076c1c3a1a727121280155c1022209aba278e65e
hendryboyz <Hendry.js1247@gmail.com>
2019-01-16 09:13:17 +0800

    Kata : FizzBuzz - 1st_simple

---
+++ Kata.Tests/FizzBuzzTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        private FizzBuzz game;
+
+        [Test]
+        public void CanCreate()
+        {
+            game = new FizzBuzz();
+            Assert.NotNull(game);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [TestCase(1)]
+        [TestCase(7)]
+        [TestCase(11)]
+        [TestCase(97)]
+        public void GivenNumber_ThenReturnNumber(int number)
+        {
+            string result = game.Given(number);
+            Assert.AreEqual(Convert.ToString(number), result);
+        }
+
+        [TestCase(3)]
+        [TestCase(6)]
+        [TestCase(9)]
+        [TestCase(21)]
+        public void GivenNumberDivisibleBy3_ThenReturnFizz(int number)
+        {
+            string result = game.Given(number);
+            StringAssert.AreEqualIgnoringCase("Fizz", result);
+        }
+
+        [TestCase(5)]
+        [TestCase(20)]
+        [TestCase(25)]
+        [TestCase(10)]
+        public void GivenNumberDivisibleBy5_ThenReturnBuzz(int number)
+        {
+            string result = game.Given(number);
+            StringAssert.AreEqualIgnoringCase("Buzz", result);
+        }
+
+        [TestCase(15)]
+        [TestCase(30)]
+        [TestCase(45)]
+        [TestCase(90)]
+        public void GivenNumberDivisibleBy3And5_ThenReturnFizzBuzz(int number)
+        {
+            string result = game.Given(number);
+            StringAssert.AreEqualIgnoringCase("FizzBuzz", result);
+        }
+    }
+}
---
+++ Kata/FizzBuzz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace Kata
+{
+    public class FizzBuzz
+    {
+        public string Given(int number)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (number % 3 == 0)
+            {
+                sb.Append("Fizz");
+            }
+            if (number % 5 == 0)
+            {
+                sb.Append("Buzz");
+            }
+            return sb.Length > 0 ? sb.ToString() : Convert.ToString(number);
+        }
+    }
+}
