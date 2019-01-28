4f1e5b21402ac3a6e45aa82b2b2cd57645a73b0a
hendryboyz <Hendry.js1247@gmail.com>
2019-01-28 09:25:46 +0800

    Kata : Number2Lcd - part1

---
+++ Kata.Tests/Number2LCDTests.cs
@@ -0,0 +1,51 @@
+﻿using Kata.Converter;
+using NUnit.Framework;
+using System;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class Number2LCDTests
+    {
+        private LcdConverter converter;
+
+        [Test]
+        public void CanCreate()
+        {
+            converter = new LcdConverter();
+            Assert.NotNull(converter);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void CanConvertDigit()
+        {
+            string result = converter.Convert(1);
+            Console.WriteLine(result);
+
+            Assert.AreEqual("   \n  |\n  |", result);
+        }
+
+        [TestCase(2, " _ \n _|\n|_ ")]
+        [TestCase(3, " _ \n _|\n _|")]
+        [TestCase(4, "   \n|_|\n  |")]
+        [TestCase(5, " _ \n|_ \n _|")]
+        [TestCase(6, "   \n|_ \n|_|")]
+        [TestCase(7, " _ \n  |\n  |")]
+        [TestCase(8, " _ \n|_|\n|_|")]
+        [TestCase(9, " _ \n|_|\n _|")]
+        [TestCase(0, " _ \n| |\n|_|")]
+        public void GivenDigit_WhenConvert_ThenReturnLcdFormat(int digit, string expected)
+        {
+            string result = converter.Convert(digit);
+            Console.WriteLine(result);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Kata/Converter/LcdConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace Kata.Converter
+{
+    public class LcdConverter
+    {
+        private readonly string[] lcdNumbers = new string[] 
+        {
+            " _ \n| |\n|_|",
+            "   \n  |\n  |",
+            " _ \n _|\n|_ ",
+            " _ \n _|\n _|",
+            "   \n|_|\n  |",
+            " _ \n|_ \n _|",
+            "   \n|_ \n|_|",
+            " _ \n  |\n  |",
+            " _ \n|_|\n|_|",
+            " _ \n|_|\n _|"
+        };
+        public string Convert(int digit, int widthScale = 1, int heightScale = 1)
+        {
+            return lcdNumbers[digit];
+        }
+    }
+}
