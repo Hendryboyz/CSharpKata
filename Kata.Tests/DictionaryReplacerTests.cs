4fc1e2af4b3e4402494336b0c2d16e024509a15f
hendryboyz <Hendry.js1247@gmail.com>
2019-02-26 09:50:32 +0800

    Kata : Dictionary Replacer - 1st

---
+++ Kata.Tests/DictionaryReplacerTests.cs
@@ -0,0 +1,67 @@
+﻿using Kata.Replacer;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class DictionaryReplacerTests
+    {
+        private DictionaryReplacer replacer;
+
+        [Test]
+        public void CanCreate()
+        {
+            replacer = new DictionaryReplacer();
+            Assert.NotNull(replacer);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void CanHandle()
+        {
+            IDictionary<string, string> dictionary = new Dictionary<string, string>();
+            string input = string.Empty;
+            string result = replacer.Handle(input, dictionary);
+            Assert.AreEqual("", result);
+        }
+
+        [TestCase("$greeting$ world", new string[] { "greeting,hello" }, ExpectedResult = "hello world")]
+        [TestCase("$greeting$ $target$", new string[] {
+            "greeting,hello", "target,world"
+        }, ExpectedResult = "hello world")]
+        public string GivenInputStringAndDictionary_WhenHandle_ReturnStringAfterReplacing(
+            string input, string[] cvsDictionary)
+        {
+            IDictionary<string, string> dictionary = ConvertToDictionary(cvsDictionary);
+            return replacer.Handle(input, dictionary);
+        }
+
+        [TestCase("$greeting$ greeting world", new string[] {
+            "greeting,hello"
+        }, ExpectedResult = "hello greeting world")]
+        public string GivenInputContainKeyShouldNotReplcae_WhenHandle_ReturnStringAfterReplcaing(
+            string input, string[] cvsDictionary)
+        {
+            IDictionary<string, string> dictionary = ConvertToDictionary(cvsDictionary);
+            return replacer.Handle(input, dictionary);
+        }
+
+    private IDictionary<string, string> ConvertToDictionary(string[] cvsDictionary)
+        {
+            IDictionary<string, string> dictionary = new Dictionary<string, string>();
+            foreach (string eachCvs in cvsDictionary)
+            {
+                string[] splittedCvs = eachCvs.Split(",");
+                dictionary.Add(splittedCvs[0], splittedCvs[1]);
+            }
+            return dictionary;
+        }
+    }
+}
---
+++ Kata/Replacer/DictionaryReplacer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata.Replacer
+{
+    public class DictionaryReplacer
+    {
+        public string Handle(string input, IDictionary<string, string> dictionary)
+        {
+            return HandleByCharactorArray(input, dictionary);
+        }
+
+        private string HandleByCharactorArray(string input, IDictionary<string, string> dictionary)
+        {
+            bool findKey = false;
+            StringBuilder result = new StringBuilder();
+            StringBuilder keyBuilder = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char element = input[i];
+                if (element == '$')
+                {
+                    if (findKey)
+                    {
+                        string key = keyBuilder.ToString();
+                        result.Append(dictionary[key]);
+                    }
+                    else
+                    {
+                        keyBuilder = new StringBuilder();
+                    }
+                    findKey ^= true;
+                    continue;
+                }
+
+                if (findKey)
+                {
+                    keyBuilder.Append(element);
+                }
+                else
+                {
+                    result.Append(element);
+                }
+            }
+
+            return result.ToString(); ;
+        }
+
+        private string HandlyByStringFormat(string input, IDictionary<string, string> dictionary)
+        {
+            foreach (KeyValuePair<string, string> element in dictionary)
+            {
+                string key = element.Key;
+                string val = element.Value;
+                input = input.Replace(string.Format("${0}$", key), val);
+            }
+            return input;
+        }
+    }
+}
