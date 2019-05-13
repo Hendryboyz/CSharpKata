da5e96c0f7d5b26dbe1ad76d25adceb55d8a49bb
hendryboyz <hendry.js1247@gmail.com>
2019-05-13 10:10:22 +0800

    Code War Kata : Find the odd int

---
+++ Kata.Tests/OddIntegerTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+
+// https://www.codewars.com/kata/find-the-odd-int/train/csharp
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class OddIntegerTests
+    {
+        private OddInteger algorithm;
+
+        [Test]
+        public void CanCreate()
+        {
+            algorithm = new OddInteger();
+            algorithm.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void TestCanFind()
+        {
+            int integer = algorithm.Find(new int[] { 1 });
+            integer.Should().Be(1);
+        }
+
+        [Test]
+        public void GivenSequenceOfIntegers_WhenFind_ThenReturnOddInteger()
+        {
+            int integer = algorithm.Find(new int[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 });
+            integer.Should().Be(5);
+        }
+    }
+}
---
+++ Kata/CodeWar/OddInteger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+namespace Kata.CodeWar
+{
+    public class OddInteger
+    {
+        public int Find(int[] sequence)
+        {
+            Hashtable countTable = new Hashtable();
+            foreach (int eachInteger in sequence)
+            {
+                if (countTable.ContainsKey(eachInteger))
+                {
+                    int count = (int)countTable[eachInteger];
+                    countTable[eachInteger] = count + 1;
+                }
+                else
+                {
+                    countTable.Add(eachInteger, 1);
+                }
+            }
+
+            foreach (int eachKey in countTable.Keys)
+            {
+                int count = (int)countTable[eachKey];
+                if (IsOdd(count))
+                {
+                    return eachKey;
+                }
+            }
+            return 1;
+        }
+
+        private bool IsOdd(int count)
+        {
+            return (count & 1) == 1;
+        }
+    }
+}
