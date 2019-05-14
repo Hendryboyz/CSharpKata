83752e7af15e71a9b8fc073f500539facc8accc7
hendryboyz <hendry.js1247@gmail.com>
2019-05-14 09:46:45 +0800

    CodeWar Kata : Stock List

---
+++ Kata.Tests/StockListTests.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class StockListTests
+    {
+        private StockList list;
+
+        [Test]
+        public void CanCreate()
+        {
+            list = new StockList();
+            list.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void CanSummary()
+        {
+            string summary = list.Summary(new string[] { "ABRS 1" }, new string[] { "A" });
+            summary.Should().Be("(A : 1)");
+        }
+
+        [Test]
+        public void GivenStockMatchCategory_WhenSummary_ThenReturnSummary()
+        {
+            string summary = list.Summary(
+                new string[] {
+                    "ABAR 200",
+                    "CDXE 500",
+                    "BKWR 250",
+                    "BTSQ 890",
+                    "DRTY 600"
+                }, 
+                new string[] { "A", "B", "C", "D" });
+            summary.Should().Be("(A : 200) - (B : 1140) - (C : 500) - (D : 600)");
+        }
+
+        [Test]
+        public void GivenStockNotMatchCategory_WhenSummary_ThenOnlySummaryKnownCategory()
+        {
+            string summary = list.Summary(
+                new string[] {
+                    "ABAR 200",
+                    "CDXE 500",
+                    "BKWR 250",
+                    "BTSQ 890",
+                    "DRTY 600"
+                },
+                new string[] { "A", "B"});
+            summary.Should().Be("(A : 200) - (B : 1140)");
+        }
+
+        [TestCase(new string[] { "ABRS 1" }, new string[] { })]
+        [TestCase(new string[] { }, new string[] { "A" })]
+        public void GivenEmptyStockOrCategory_WhenSummary_ThenReturnEmptyString(
+            string[] stockQuantiy,
+            string[] categories)
+        {
+            string summary = list.Summary(stockQuantiy, categories);
+            summary.Should().BeEmpty();
+        }
+    }
+}
---
+++ Kata/CodeWar/StockList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata.CodeWar
+{
+    public class StockList
+    {
+        public string Summary(string[] stockQuantity, string[] categories)
+        {
+
+            if (stockQuantity.Length == 0 || categories.Length == 0)
+            {
+                return string.Empty;
+            }
+
+            IDictionary<string, int> stockTable = CreateStockTable(categories);
+
+            foreach (string eachStock in stockQuantity)
+            {
+                string[] stockParts = eachStock.Split(" ");
+                string stockCategory = stockParts[0].Substring(0, 1);
+                int quantity = Convert.ToInt32(stockParts[1]);
+                if (stockTable.ContainsKey(stockCategory))
+                {
+                    stockTable[stockCategory] += quantity;
+                }
+            }
+
+            string summary = string.Join(" - ",
+                stockTable.Select(x => string.Format("({0} : {1})", x.Key, x.Value)).ToArray());
+            return summary;
+        }
+
+        private IDictionary<string, int> CreateStockTable(string[] categories)
+        {
+            IDictionary<string, int> stockTable = new Dictionary<string, int>();
+            foreach (string eachCategory in categories)
+            {
+                if (!stockTable.ContainsKey(eachCategory))
+                {
+                    stockTable.Add(eachCategory, 0);
+                }
+            }
+
+            return stockTable;
+        }
+
+        private string SummaryByFunc(string[] stockQuantity, string[] categories)
+        {
+            return string.Join(" - ",
+                categories.Select(x => string.Format("({0} : {1})", x,
+                    stockQuantity.Where(s => x.Equals(s.Substring(0, 1)))
+                                 .Sum(s => Convert.ToInt16(s.Split(' ')[1]))
+                    )
+                )
+            );
+        }
+    }
+}
