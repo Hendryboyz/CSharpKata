c2a737dfa5199179e194b2ee292b3585ffdc5318
hendryboyz <hendry.js1247@gmail.com>
2019-05-15 09:41:34 +0800

    CodeWar Kata : Which are in - data structure selection

---
+++ Kata.Tests/WhichAreInTests.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+
+// https://www.codewars.com/kata/550554fd08b86f84fe000a58
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class WhichAreInTests
+    {
+        private WhichAreIn algorithm;
+
+        [Test]
+        public void CanCreate()
+        {
+            algorithm = new WhichAreIn();
+            algorithm.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void CanCheckSubset()
+        {
+            string[] result = algorithm.CheckSubset(new string[] { "a" }, new string[] { "a" });
+            result.Should().Contain("a");
+        }
+
+        [Test]
+        public void GivenSimpleCase_WhenCheckSubset_ThenReturnElementIsSubset()
+        {
+            string[] result = algorithm.CheckSubset(
+                new string[] { "arp", "live", "strong" }, 
+                new string[] { "lively", "alive", "harp", "sharp", "armstrong" }
+            );
+            result.Should().Equal(new string[] { "arp", "live", "strong" });
+        }
+
+        [Test]
+        public void GivenAllNotValidSubset_WhenCheckSubset_ThenReturnEmptyArray()
+        {
+            string[] result = algorithm.CheckSubset(
+                new string[] { "marp", "clive", "tstrong" },
+                new string[] { "lively", "alive", "harp", "sharp", "armstrong" }
+            );
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void GivenDuplicatedSubset_WhenCheckSubset_ThenReturnThatSubsetSingleTime()
+        {
+            string[] result = algorithm.CheckSubset(
+                new string[] { "arp", "live", "strong", "arp" },
+                new string[] { "lively", "alive", "harp", "sharp", "armstrong" }
+            );
+            result.Should().Equal(new string[] { "arp", "live", "strong" });
+        }
+
+        [Test]
+        public void GivenUnorderedSubset_WhenCheckSubset_ThenReturnOrderSubset()
+        {
+            string[] result = algorithm.CheckSubset(
+                new string[] { "strong", "live", "arp" },
+                new string[] { "lively", "alive", "harp", "sharp", "armstrong" }
+            );
+            result.Should().Equal(new string[] { "arp", "live", "strong" });
+        }
+    }
+}
---
+++ Kata/CodeWar/WhichAreIn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata.CodeWar
+{
+    public class WhichAreIn
+    {
+        public string[] CheckSubset(string[] subset, string[] stringSequence)
+        {
+            SortedSet<string> validSubset = new SortedSet<string>();
+            foreach (string eachSub in subset)
+            {
+                foreach (string eachString in stringSequence)
+                {
+                    if (eachString.Contains(eachSub))
+                    {
+                        validSubset.Add(eachSub);
+                        break;
+                    }
+                }
+            }
+            return validSubset.ToArray();
+        }
+    }
+}
