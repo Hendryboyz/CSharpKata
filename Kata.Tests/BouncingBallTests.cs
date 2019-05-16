17fb01b833f78098418f1abbefe5e4cefebb2c70
hendryboyz <hendry.js1247@gmail.com>
2019-05-16 09:53:51 +0800

    CodeWar Kata : BouncingBall - algorithm

---
+++ Kata.Tests/BouncingBallTests.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Kata.Game;
+using NUnit.Framework;
+
+// https://www.codewars.com/kata/bouncing-balls
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class BouncingBallTests
+    {
+        private BouncingBall game;
+
+        [Test]
+        public void CanCreate()
+        {
+            game = new BouncingBall();
+            game.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void CanExpectBouncingTimes()
+        {
+            int times = game.ExpectBouncingTimes(3, 0.66, 1.5);
+            times.Should().Be(3);
+        }
+
+        [Test]
+        public void GivenBallHeight30_WhenExpectBouncingTimes_ThenReturn15()
+        {
+            int times = game.ExpectBouncingTimes(30, 0.66, 1.5);
+            times.Should().Be(15);
+        }
+
+        [Test]
+        public void GivenBallHeight3AndBounceFactorHalf_WhenExpectedBouncingTimes_ThenReturn2()
+        {
+            int times = game.ExpectBouncingTimes(3, 0.5, 1.5);
+            times.Should().Be(2);
+        }
+
+        [TestCase(-1, 0.66, 1.5)]
+        [TestCase(3, 1, 1.5)]
+        [TestCase(3, 0, 1.5)]
+        [TestCase(3, 0.66, 3)]
+        public void GivenLackOfParameters_WhenExpectedBouncingTimes_ThenReturnMinusOne(
+            double ballHeight, double bounceFactor, double windowHeight)
+        {
+            int times = game.ExpectBouncingTimes(ballHeight, bounceFactor, windowHeight);
+            times.Should().Be(-1);
+        }
+    }
+}
---
+++ Kata/Game/BouncingBall.cs
@@ -0,0 +1,34 @@
+﻿namespace Kata.Game
+{
+    public class BouncingBall
+    {
+        public int ExpectBouncingTimes(double ballHeight, double bounceFactor, double windowHeight)
+        {
+            if (!IsValidParameters(ballHeight, bounceFactor, windowHeight))
+            {
+                return -1;
+            }
+            int times = 1;
+            while (ballHeight >= windowHeight)
+            {
+                ballHeight *= bounceFactor;
+                if (ballHeight > windowHeight)
+                {
+                    times += 2;
+                }
+                else if (ballHeight == windowHeight)
+                {
+                    times += 1;
+                }
+            }
+            return times;
+        }
+
+        private bool IsValidParameters(double ballHeight, double bounceFactor, double windowHeight)
+        {
+            return ballHeight > 0 && 
+                (0 < bounceFactor && bounceFactor < 1) &&
+                windowHeight < ballHeight;
+        }
+    }
+}
