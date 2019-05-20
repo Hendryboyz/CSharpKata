5dabc76ff475e4ed3cb57067a6b74f1b06c7fdbf
hendryboyz <hendry.js1247@gmail.com>
2019-05-20 11:01:26 +0800

    Codewar Kata : Playpass - only character case

---
+++ Kata.Tests/PlayPassTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class PlayPassTests
+    {
+        private PlayPass playPass;
+
+        [Test]
+        public void CanCreate()
+        {
+            playPass = new PlayPass();
+            playPass.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void CanConvert()
+        {
+            string result = playPass.Convert("A", 1);
+            result.Should().Be("b");
+        }
+
+        [Test]
+        public void GivenATOZ_WhenConvert_ThenCircularShift()
+        {
+            string result = playPass.Convert("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 1);
+            result.Should().Be("AzYxWvUtSrQpOnMlKjIhGfEdCb");
+        }
+    }
+}
---
+++ Kata/CodeWar/PlayPass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+// https://www.codewars.com/kata/559536379512a64472000053
+namespace Kata.CodeWar
+{
+    public class PlayPass
+    {
+        public string Convert(string password, int shift)
+        {
+            string result = CircularShiftPassword(password, shift);
+            result = DownCaseOddLetter(result);
+            result = string.Join("", result.Reverse());
+
+            return result;
+        }
+
+        private string DownCaseOddLetter(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int passwordIdx = 0; passwordIdx < password.Length; passwordIdx++)
+            {
+                char eachCharacter = password[passwordIdx];
+                if (char.IsLetter(eachCharacter) && (passwordIdx & 1) == 0)
+                {
+                    sb.Append(char.ToLower(eachCharacter));
+                }
+                else
+                {
+                    sb.Append(eachCharacter);
+                }
+            }
+            return sb.ToString();
+        }
+
+        private string CircularShiftPassword(string password, int shift)
+        {
+            StringBuilder shiftedPassword = new StringBuilder();
+            foreach (char eachCharacter in password)
+            {
+                if (char.IsLetter(eachCharacter))
+                {
+                    char result = (char)(eachCharacter + shift);
+                    if (result > 'Z')
+                    {
+                        shiftedPassword.Append((char)(result % 'Z' + ('A' - 1)));
+                    }
+                    else
+                    {
+                        shiftedPassword.Append(result);
+                    }
+                }
+            }
+            return shiftedPassword.ToString();
+        }
+    }
+}
