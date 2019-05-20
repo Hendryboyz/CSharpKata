662353c780a67842aae7015d429cccd8b14370eb
hendryboyz <hendry.js1247@gmail.com>
2019-05-20 09:26:08 +0800

    Codewar Kata : The Supermarket Queue - algorithm

---
+++ Kata.Tests/MyThreadPoolTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+
+// https://www.codewars.com/kata/the-supermarket-queue
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class MyThreadPoolTests
+    {
+        private MyThreadPool pool;
+
+        [Test]
+        public void CanCreate()
+        {
+            pool = new MyThreadPool();
+            pool.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void CanCheckQueueTime()
+        {
+            int times = pool.CheckQueueTime(new int[] { }, 1);
+            times.Should().Be(0);
+        }
+
+        [Test]
+        public void GivenThreeTaskAndOneThread_ThenCheckQueueTime_ThenReturnTimeSummary()
+        {
+            int times = pool.CheckQueueTime(new int[] { 10, 2, 3 }, 1);
+            times.Should().Be(15);
+        }
+
+        [TestCase(new int[] { 10, 2 }, 2, 10)]
+        [TestCase(new int[] { 2, 3, 10 }, 2, 12)]
+        public void GivenTwoThreadAndTwoTasks_ThenCheckQueueTime_ThenReturnTotalTimes(
+            int[] taskArray, int threadNumber, int expected)
+        {
+            int times = pool.CheckQueueTime(taskArray, threadNumber);
+            times.Should().Be(expected);
+        }
+
+        [Test]
+        public void GivenTwoThreadAndOrderedTasks_ThenCheckQueueTime_ThenReturnTotalTimes()
+        {
+            int times = pool.CheckQueueTime(new int[] { 2, 2, 3, 3, 4, 4 }, 2);
+            times.Should().Be(9);
+        }
+
+        [Test]
+        public void GivenThreeThreadAndThreeTasks_ThenCheckQueueTime_ThenReturnTotalTimes()
+        {
+            int times = pool.CheckQueueTime(new int[] { 5, 2, 3 }, 3);
+            times.Should().Be(5);
+        }
+    }
+}
---
+++ Kata/CodeWar/MyThreadPool.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata.CodeWar
+{
+    public class MyThreadPool
+    {
+        public int CheckQueueTime(int[] taskArray, int threadNumber)
+        {
+            int[] threads = new int[threadNumber];
+            foreach (int taskConsume in taskArray)
+            {
+                int minTimeConsumeThread = threads.ToList().FindIndex(x => x == threads.Min());
+                threads[minTimeConsumeThread] += taskConsume;
+            }
+            return threads.Max();
+        }
+
+        private int LoopWithQueue(int[] taskArray, int threadNumber)
+        {
+            Queue<int> taskQueue = new Queue<int>(taskArray);
+            int[] pools = new int[threadNumber];
+            int timeConsumes = 0;
+            while (taskQueue.Count > 0 || pools.Count(x => x > 0) > 0)
+            {
+                for (int poolIndex = 0; poolIndex < threadNumber; poolIndex++)
+                {
+                    if (0 == pools[poolIndex] && 0 < taskQueue.Count)
+                    {
+                        pools[poolIndex] = taskQueue.Dequeue();
+                    }
+                    if (0 < pools[poolIndex])
+                    {
+                        pools[poolIndex]--;
+                    }
+                }
+                timeConsumes++;
+            }
+            return timeConsumes;
+        }
+    }
+}
