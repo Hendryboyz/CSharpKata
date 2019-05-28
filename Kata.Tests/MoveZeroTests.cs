5377e82d965ffccace9e61677a2e21e1b6b5702f
hendryboyz <hendry.js1247@gmail.com>
2019-05-28 09:37:32 +0800

    Codewar Kata : Moving Zeros To The End

---
+++ Kata.Tests/MoveZeroTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class MoveZeroTests
+    {
+        private MoveZero algorithm;
+
+        [Test]
+        public void CanCreate()
+        {
+            algorithm = new MoveZero();
+            algorithm.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void CanMoveToLast()
+        {
+            int[] result = algorithm.MoveToLast(new int[] { 0 });
+            result.Should().Equal(new int[] { 0 });
+        }
+
+        [Test]
+        public void GivenStartWithZero_WhenMoveToLast_ThenReturnZeroAtLast()
+        {
+            int[] result = algorithm.MoveToLast(new int[] { 0, 1 });
+            result.Should().Equal(new int[] { 1, 0 });
+        }
+
+        [Test]
+        public void GivenNormalCase_WhenMoveToLast_ThenReturnAllZeroAtLast()
+        {
+            int[] result = algorithm.MoveToLast(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 });
+            result.Should().Equal(new int[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 });
+        }
+
+        [Test]
+        public void GivenContinuousZero_WhenMoveToLast_ThenReturnAllZeroAtLast()
+        {
+            int[] result = algorithm.MoveToLast(new int[] { 0, 0, 3 });
+            result.Should().Equal(new int[] { 3, 0, 0 });
+        }
+    }
+}
---
+++ Kata/CodeWar/MoveZero.cs
@@ -0,0 +1,31 @@
+﻿namespace Kata.CodeWar
+{
+    public class MoveZero
+    {
+        public int[] MoveToLast(int[] intArray)
+        {
+            for (int index = 0; index < intArray.Length; index++)
+            {
+                bool isZero = (0 == intArray[index]);
+                int swappingTimes = 0;
+                while (isZero && swappingTimes < intArray.Length - index)
+                {
+                    intArray = SwapToLast(intArray, index);
+                    swappingTimes++;
+                    isZero = (0 == intArray[index]);
+                }
+            }
+            return intArray;
+        }
+
+        private int[] SwapToLast(int[] intArray, int swappingIndex)
+        {
+            for (int index = swappingIndex; index < intArray.Length - 1; index++)
+            {
+                intArray[index] = intArray[index + 1];
+            }
+            intArray[intArray.Length - 1] = 0;
+            return intArray;
+        }
+    }
+}
