6eb8f0c3ef6eb08ec28866df55aa2c0868135015
hendryboyz <hendry.js1247@gmail.com>
2019-05-29 09:49:01 +0800

    Codewar Kata : Weight for weight - bubble sorting

---
+++ Kata.Tests/WeightSortTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+
+// https://www.codewars.com/kata/weight-for-weight/csharp
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class WeightSortTests
+    {
+        private WeightSort algorithm;
+
+        [Test]
+        public void CanCreate()
+        {
+            algorithm = new WeightSort();
+            algorithm.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void TestCanSort()
+        {
+            string result = algorithm.Sort("1 2 3");
+            result.Should().Be("1 2 3");
+        }
+
+        [Test]
+        public void WhenGivenTwoDifferentWeightNumber_WhenSort_ThenReturnResult()
+        {
+            string result = algorithm.Sort("99 100");
+            result.Should().Be("100 99");
+        }
+
+        [Test]
+        public void WhenGivenTwoSameWeightButDifferentNumber_WhenSort_ThenReturnResult()
+        {
+            string result = algorithm.Sort("65 56");
+            result.Should().Be("56 65");
+        }
+
+        [TestCase("56 65 74 100 99 68 86 180 90", "100 180 90 56 65 74 68 86 99")]
+        [TestCase("103 123 4444 99 2000", "2000 103 123 4444 99")]
+        [TestCase("2000 10003 1234000 44444444 9999 11 11 22 123", "11 11 2000 10003 22 123 1234000 44444444 9999")]
+        public void WhenGivenNormalList_WhenSort_ThenReturnResult(string numbers, string expected)
+        {
+            string result = algorithm.Sort(numbers);
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ Kata/CodeWar/WeightSort.cs
@@ -0,0 +1,61 @@
+﻿namespace Kata.CodeWar
+{
+    public class WeightSort
+    {
+        public string Sort(string weightList)
+        {
+            string[] weights = weightList.Split(" ");
+            for (int i = 0; i < weights.Length - 1; i++)
+            {
+                for (int j = 0; j < weights.Length - i - 1; j++)
+                {
+                    if (CompareWeightDigit(weights, j, j + 1) > 0)
+                    {
+                        Swap(weights, j, j + 1);
+                    }
+                }
+            }
+            return string.Join(" ", weights);
+        }
+
+        private int CompareWeightDigit(string[] weights, int source, int destination)
+        {
+            if (DigitSum(weights[source]) > DigitSum(weights[destination]))
+            {
+                return 1;
+            }
+            else if (DigitSum(weights[source]) < DigitSum(weights[destination]))
+            {
+                return -1;
+            }
+            else
+            {
+                if (weights[source].CompareTo(weights[destination]) > 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        private int DigitSum(string number)
+        {
+            int sum = 0;
+            foreach (char digit in number)
+            {
+                sum += digit - '0';
+            }
+            return sum;
+        }
+
+        private void Swap(string[] weights, int source, int destination)
+        {
+            string tmp = weights[source];
+            weights[source] = weights[destination];
+            weights[destination] = tmp;
+        }
+    }
+}
