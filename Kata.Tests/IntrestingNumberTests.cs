1504e4b3ed5c22085569f7da2b7224b030ffd8ba
hendryboyz <hendry.js1247@gmail.com>
2019-05-30 09:53:00 +0800

    Codewar Kata : Interesting Number

---
+++ Kata.Tests/IntrestingNumberTests.cs
@@ -0,0 +1,91 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+// https://www.codewars.com/kata/catching-car-mileage-numbers/
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class IntrestingNumberTests
+    {
+        private IntrestingNumber intrestingNumber;
+
+        [Test]
+        public void CanCreate()
+        {
+            intrestingNumber = new IntrestingNumber();
+            intrestingNumber.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void CanCheck()
+        {
+            int isIntresting = intrestingNumber.Check(99, new List<int>() { 1337, 256 });
+            isIntresting.Should().Be(0);
+        }
+
+        [Test]
+        public void GivenLessThan100Number_WhenCheck_ThenReturn0()
+        {
+            for (int number = 0; number < 100; number++)
+            {
+                int isIntresting = intrestingNumber.Check(99, new List<int>() { 99, 256 });
+                isIntresting.Should().Be(0);
+            }
+        }
+
+        [TestCase(0)]
+        [TestCase(1000000000)]
+        public void GivenOutOfRangeNumber_WhenCheck_ThenThrowArgumentException(int number)
+        {
+            Exception ex = Assert.Throws<ArgumentException>(() => intrestingNumber.Check(number, new List<int>()));
+
+            ex.Should().NotBeNull();
+            ex.Should().BeOfType<ArgumentException>();
+        }
+
+        [Test]
+        public void GivenNumberInAwesomePhrases_WhenCheck_ThenReturn2()
+        {
+            int isIntresting = intrestingNumber.Check(1337, new List<int>() { 1337, 256 });
+            isIntresting.Should().Be(2);
+            isIntresting = intrestingNumber.Check(256, new List<int>() { 1337, 256 });
+            isIntresting.Should().Be(2);
+        }
+
+        [TestCase(100)]
+        [TestCase(2000)]
+        [TestCase(4000000)]
+        public void GivenNumberOnlyFirstDigitNotZero_WhenCheck_ThenReturn2(int number)
+        {
+            int isIntresting = intrestingNumber.Check(number, new List<int>());
+            isIntresting.Should().Be(2);
+        }
+
+        [TestCase(111)]
+        [TestCase(2222)]
+        [TestCase(33333)]
+        [TestCase(555555555)]
+        public void GivenNumberOnlyHaveTheSameDigit_WhenCheck_ThenReturn2(int number)
+        {
+            int isIntresting = intrestingNumber.Check(number, new List<int>());
+            isIntresting.Should().Be(2);
+        }
+
+        [TestCase(1221)]
+        [TestCase(73837)]
+        public void GivenPalindromeNumber_WhenCheck_ThenReturn2(int number)
+        {
+            int isIntresting = intrestingNumber.Check(number, new List<int>());
+            isIntresting.Should().Be(2);
+        }
+    }
+}
---
+++ Kata/CodeWar/IntrestingNumber.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kata.CodeWar
+{
+    public class IntrestingNumber
+    {
+        public int Check(int number, List<int> awesomePhrases)
+        {
+            CheckNumberRange(number);
+            if (number < 100)
+            {
+                return 0;
+            }
+
+            int isIntresting = 0;
+            if (IsOnlyFirstDigitNotZero(number) ||
+                IsAllDigitsTheSame(number) ||
+                IsPalindrome(number) ||
+                awesomePhrases.Contains(number))
+            {
+                isIntresting = 2;
+            }
+
+            return isIntresting;
+        }
+
+        private bool IsPalindrome(int number)
+        {
+            char[] digits = Convert.ToString(number).ToCharArray();
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if ((digits[digits.Length - 1 - i] - digits[i]) != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool IsAllDigitsTheSame(int number)
+        {
+            char[] digits = Convert.ToString(number).ToCharArray();
+            char digit = digits[0];
+            for (int i = 1; i < digits.Length; i++)
+            {
+                if ((digits[i] - digit) != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void CheckNumberRange(int number)
+        {
+            if (number <= 0 || 1000000000 <= number)
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        private bool IsOnlyFirstDigitNotZero(int number)
+        {
+            char[] digits = Convert.ToString(number).ToCharArray();
+            int digitSum = 0;
+            for (int i = 1; i < digits.Length; i++)
+            {
+                digitSum += (digits[i] - '0');
+            }
+            if (0 == digitSum)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
