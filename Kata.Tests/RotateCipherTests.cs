cf6621c63d898e828bce346f8f296e7d42c78fb4
hendryboyz <hendry.js1247@gmail.com>
2019-06-03 09:22:51 +0800

    Codewar Kata : Rotate 13 - finished

---
+++ Kata.Tests/RotateCipherTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+
+// https://www.codewars.com/kata/rot13-1/csharp
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class RotateCipherTests
+    {
+        private RotateCipher cipher;
+
+        [Test]
+        public void TestCanCreate()
+        {
+            cipher = new RotateCipher();
+            cipher.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            TestCanCreate();
+        }
+
+        [Test]
+        public void CanRotate()
+        {
+            string result = cipher.Rotate("0");
+            result.Should().Be("0");
+        }
+
+        [TestCase("0123456789", "0123456789")]
+        [TestCase("!@#$%^&*()_+", "!@#$%^&*()_+")]
+        public void GivenNotLetters_ThenRotate_ReturnOriginal(string original, string expected)
+        {
+            string result = cipher.Rotate(original);
+            result.Should().Be(expected);
+        }
+        
+        [Test]
+        public void GivenOnlyLowerLetters_ThenRotate_ReturnResult()
+        {
+            string result = cipher.Rotate("test");
+            result.Should().Be("grfg");
+        }
+
+        [Test]
+        public void GivenOnlyUpperLetters_ThenRotate_ReturnResult()
+        {
+            string result = cipher.Rotate("TEST");
+            result.Should().Be("GRFG");
+        }
+
+        [Test]
+        public void GivenMixedLetters_ThenRotate_ReturnResult()
+        {
+            string result = cipher.Rotate("Test");
+            result.Should().Be("Grfg");
+        }
+    }
+}
---
+++ Kata/CodeWar/RotateCipher.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace Kata.CodeWar
+{
+    public class RotateCipher
+    {
+        public string Rotate(string original)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (char eachCharacter in original.ToCharArray())
+            {
+                if (char.IsLetter(eachCharacter))
+                {
+                    result.Append(RotateLetters(eachCharacter));
+                }
+                else
+                {
+                    result.Append(eachCharacter);
+                }
+            }
+            return result.ToString();
+        }
+
+        private char RotateLetters(char originalCharacter)
+        {
+            string rotateLetters = "nopqrstuvwxyzabcdefghijklm";
+
+            if (char.IsUpper(originalCharacter))
+            {
+                int letterIndex = char.ToLower(originalCharacter) - 'a';
+                return char.ToUpper(rotateLetters[letterIndex]);
+            }
+            else
+            {
+                int letterIndex = originalCharacter - 'a';
+                return rotateLetters[letterIndex];
+            }
+        }
+    }
+}
