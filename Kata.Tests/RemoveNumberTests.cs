3341b3c6758e0557a87bb04ab64a398920842a6b
hendryboyz <hendry.js1247@gmail.com>
2019-06-04 10:27:54 +0800

    Codewar Kata : Remove Number
     * need O(N) solution

---
+++ Kata.Tests/RemoveNumberTests.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+// https://www.codewars.com/kata/is-my-friend-cheating
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class RemoveNumberTests
+    {
+        private RemoveNumber removeNumber;
+
+        [Test]
+        public void CanCreate()
+        {
+            removeNumber = new RemoveNumber();
+            removeNumber.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void CanRemove()
+        {
+            List<long[]> possibleRemoval = removeNumber.Remove(1);
+            possibleRemoval.Should().BeEquivalentTo(new List<long[]>() {
+            });
+        }
+
+        [Test]
+        public void GivenLessThanOneNumber_WhenRemove_ThenThrowArgumentException()
+        {
+            Exception ex = Assert.Catch<ArgumentException>(() => removeNumber.Remove(0));
+
+            ex.Should().NotBeNull();
+            ex.Should().BeOfType<ArgumentException>();
+        }
+
+        [Test]
+        public void GivenSimpleCase_WhenRemove_ThenReturnPossibleRemoval()
+        {
+            List<long[]> possibleRemoval = removeNumber.Remove(26);
+            possibleRemoval.Should().BeEquivalentTo(new List<long[]>()
+            {
+                new long[] { 15, 21 },
+                new long[] { 21, 15 }
+            });
+        }
+
+        [Test]
+        public void GivenHundred_WhenRemove_ThenReturnEmptyList()
+        {
+            List<long[]> possibleRemoval = removeNumber.Remove(100);
+            possibleRemoval.Should().BeEquivalentTo(new List<long[]>()
+            {
+            });
+        }
+
+        [Test, MaxTime(12000)]
+        public void GivenLargeNumber_WhenRemove_ThenReturnNotEmptyListInPeriodTime()
+        {
+            List<long[]> possibleRemoval = removeNumber.Remove(int.MaxValue);
+            possibleRemoval.Should().NotBeEmpty();
+        }
+    }
+}
---
+++ Kata/CodeWar/RemoveNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kata.CodeWar
+{
+    public class RemoveNumber
+    {
+        public List<long[]> Remove(long number)
+        {
+            List<long[]> possibleRemoval = new List<long[]>();
+            ValidateArgument(number);
+            long sum = (number + 1) * number / 2;
+            for (long i = 1; i <= number; i++)
+            {
+                for (long j = 1; j <= number; j++)
+                {
+                    if (j == i)
+                    {
+                        continue;
+                    }
+                    if (i * j == (sum - i - j))
+                    {
+                        possibleRemoval.Add(new long[] { i, j });
+                    }
+                }
+            }
+            return possibleRemoval;
+        }
+
+        private void ValidateArgument(long number)
+        {
+            if (number < 1)
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
