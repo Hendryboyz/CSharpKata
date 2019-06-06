dbfe28712a99078f81c1c1a23a3e33ba0281738f
hendryboyz <hendry.js1247@gmail.com>
2019-06-06 09:36:37 +0800

    Codewar Kata : PigIt

---
+++ Kata.Tests/PigItTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class PigItTests
+    {
+        private PigIt algorithm;
+
+        [Test]
+        public void CanCreate()
+        {
+            algorithm = new PigIt();
+            algorithm.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void CanMoveAndAppend()
+        {
+            string result = algorithm.MoveAndAppend("it");
+            result.Should().Be("tiay");
+        }
+
+        [TestCase("Pig", "igPay")]
+        [TestCase("happy", "appyhay")]
+        public void GivenSingleWord_WhenMoveAndAppend_ThenMoveFirstToLastNAppenday(string word, string expected)
+        {
+            string result = algorithm.MoveAndAppend(word);
+            result.Should().Be(expected);
+        }
+
+        [TestCase("Pig latin is cool", "igPay atinlay siay oolcay")]
+        [TestCase("This is my string", "hisTay siay ymay tringsay")]
+        public void GivenSentence_WhenMoveAndAppend_ThenAllWordMoveFirstToLast(string sentence, string expected)
+        {
+            string result = algorithm.MoveAndAppend(sentence);
+            result.Should().Be(expected);
+        }
+
+        [TestCase("Hello world !", "elloHay orldway !")]
+        public void GivenSentenceContainpunctuation_WhenMoveAndAppend_ThenAllWordMoveFirstToLast(
+            string sentence, string expected)
+        {
+            string result = algorithm.MoveAndAppend(sentence);
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ Kata/CodeWar/PigIt.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata.CodeWar
+{
+    public class PigIt
+    {
+        private readonly string SUFFIX = "ay";
+
+        public string MoveAndAppend(string sentence)
+        {
+            List<string> words = new List<string>();
+            foreach (string eachWord in sentence.Split(" "))
+            {
+                if (IsPunctuation(eachWord))
+                {
+                    words.Add(eachWord);
+                }
+                else
+                {
+                    words.Add(HandleWord(eachWord));
+                }
+            }
+            return string.Join(" ", words);
+        }
+
+        private bool IsPunctuation(string eachWord)
+        {
+            return eachWord.Length == 1 && char.IsPunctuation(eachWord[0]);
+        }
+
+        private string HandleWord(string word)
+        {
+            StringBuilder sb = new StringBuilder(word.Substring(1));
+            sb.Append(word[0]);
+            sb.Append(SUFFIX);
+            return sb.ToString();
+        }
+    }
+}
