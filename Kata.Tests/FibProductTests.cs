454d0fd9320ecaad4ce6b865981ae2ecf314f8c8
hendryboyz <hendry.js1247@gmail.com>
2019-06-10 13:35:48 +0800

    Codewar Kata : FibProduct

---
+++ Kata.Tests/FibProductTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+using System;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class FibProductTests
+    {
+        private FibProduct algorithm;
+
+        [Test]
+        public void CanCreate()
+        {
+            algorithm = new FibProduct();
+            algorithm.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        [Timeout(1000)]
+        public void BuildFibnacci()
+        {
+            int count = 1000;
+            ulong[] fibSequence = new ulong[count + 1];
+            ulong number = algorithm.BuildFibnacci(count, fibSequence);
+            number.Should().BeGreaterThan(0);
+            Console.WriteLine(number);
+            Console.WriteLine(string.Join(", ", fibSequence));
+        }
+
+        [Test]
+        public void CanProductFib()
+        {
+            ulong[] result = algorithm.ProductFib(1);
+            result.Should().Equal(new long[] { 1, 1, 1 });
+        }
+
+        [TestCase(4895, new ulong[] { 55, 89, 1 })]
+        [TestCase(714, new ulong[] { 21, 34, 1 })]
+        public void GivenProductCase_WhenProductFib_ThenReturnFibNumAndTrue(
+            int product, ulong[] expected)
+        {
+            ulong[] result = algorithm.ProductFib(Convert.ToUInt64(product));
+            Console.WriteLine(string.Join(",", result));
+            result.Should().Equal(expected);
+        }
+
+        [TestCase(800, new ulong[] { 34, 55, 0 })]
+        public void GivenNotProductCase_WhenProductFib_ThenReturnFibNumAndTrue(
+            int product, ulong[] expected)
+        {
+            ulong[] result = algorithm.ProductFib(Convert.ToUInt64(product));
+            Console.WriteLine(string.Join(",", result));
+            result.Should().Equal(expected);
+        }
+    }
+}
---
+++ Kata/CodeWar/FibProduct.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Kata.CodeWar
+{
+    public class FibProduct
+    {
+        private readonly int SEQUECE_LENGTH = 1000;
+        private ulong[] _fedSequence;
+
+        public FibProduct()
+        {
+        }
+
+        public ulong[] ProductFib(ulong product)
+        {
+            _fedSequence = new ulong[SEQUECE_LENGTH + 1];
+            BuildFibnacci(SEQUECE_LENGTH, _fedSequence);
+
+            int index = 1;
+            while (_fedSequence[index] * _fedSequence[index - 1] < product)
+            {
+                index++;
+            }
+            ulong isEqual = _fedSequence[index] * _fedSequence[index - 1] == product ? 1UL : 0UL;
+            return new ulong[] { _fedSequence[index - 1], _fedSequence[index], isEqual };
+        }
+
+        public ulong BuildFibnacci(int index, ulong[] fibSequence)
+        {
+            if (0 == index || 1 == index)
+            {
+                fibSequence[index] = Convert.ToUInt64(index);
+                return Convert.ToUInt64(index);
+            }
+            else if (0 == fibSequence[index])
+            {
+                fibSequence[index] = BuildFibnacci(index - 1, fibSequence) + BuildFibnacci(index - 2, fibSequence);
+                return fibSequence[index];
+            }
+            else
+            {
+                return fibSequence[index];
+            }
+        }
+    }
+}
