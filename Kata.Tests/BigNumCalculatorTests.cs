b7ffebc4aa4a2296be484af4066e37eabe1820f5
hendryboyz <hendry.js1247@gmail.com>
2019-06-11 15:51:50 +0800

    Codewar Kata : Adding Big Numbers

---
+++ Kata.Tests/BigNumCalculatorTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class BigNumCalculatorTests
+    {
+        private BigNumCalculator calculator;
+
+        [Test]
+        public void CanCreate()
+        {
+            calculator = new BigNumCalculator();
+            calculator.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void CanAdd()
+        {
+            string sum = calculator.Add("1", "1");
+            sum.Should().Be("2");
+        }
+
+        [TestCase("3", "4", "7")]
+        [TestCase("21", "25", "46")]
+        [TestCase("123", "456", "579")]
+        public void GivenNotNeedCarryCase_ThenAdd_ReturnSummary(
+            string augend, string addend, string sum)
+        {
+            string result = calculator.Add(augend, addend);
+            result.Should().Be(sum);
+        }
+
+        [TestCase("5", "5", "10")]
+        [TestCase("15", "15", "30")]
+        [TestCase("85", "15", "100")]
+        public void GivenHavingCarryCase_ThenAdd_ReturnSummary(
+            string augend, string addend, string sum)
+        {
+            string result = calculator.Add(augend, addend);
+            result.Should().Be(sum);
+        }
+
+        [TestCase("15", "5", "20")]
+        [TestCase("95", "5", "100")]
+        [TestCase("5", "95", "100")]
+        [TestCase("999998", "2", "1000000")]
+        public void GivenImbalanceNum_ThenAdd_ReturnSummary(
+            string augend, string addend, string sum)
+        {
+            string result = calculator.Add(augend, addend);
+            result.Should().Be(sum);
+        }
+    }
+}
---
+++ Kata/CodeWar/BigNumCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Kata.CodeWar
+{
+    public class BigNumCalculator
+    {
+        public string Add(string augend, string addend)
+        {
+            if (addend.Length > augend.Length)
+            {
+                string temp = augend;
+                augend = addend;
+                addend = temp;
+            }
+            Stack<char> augendDigits = new Stack<char>(augend.ToCharArray());
+            Stack<char> addendDigits = new Stack<char>(addend.ToCharArray());
+            StringBuilder sum = new StringBuilder();
+            int carry = 0;
+            while (augendDigits.Count > 0)
+            {
+                int augendInt = augendDigits.Pop() - '0';
+                int addendInt = 0;
+                if (addendDigits.Count > 0)
+                {
+                    addendInt = addendDigits.Pop() - '0';
+                }
+                int digitSum = augendInt + addendInt + carry;
+                int digit = digitSum % 10;
+                carry = digitSum / 10;
+                sum.Insert(0, digit);
+            }
+            if (carry > 0)
+            {
+                sum.Insert(0, carry);
+            }
+            return sum.ToString();
+        }
+    }
+}
