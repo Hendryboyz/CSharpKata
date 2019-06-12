62f5f21781f5025fc23e3c42b8364f0d364fabba
hendryboyz <hendry.js1247@gmail.com>
2019-06-12 13:20:41 +0800

    Codewar kata : int32 to IPv4

---
+++ Kata.Tests/IpConverterTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class IpConverterTests
+    {
+        private IpConverter converter;
+
+        [Test]
+        public void CanCreate()
+        {
+            converter = new IpConverter();
+            converter.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void CanConvert()
+        {
+            string ip = converter.Convert(0U);
+            ip.Should().Be("0.0.0.0");
+        }
+
+        [TestCase(32U, "0.0.0.32")]
+        [TestCase(2154959208U, "128.114.17.104")]
+        [TestCase(2149583361U, "128.32.10.1")]
+        public void GivenSimpleCase_WhenConvert_ThenReturnIp(uint intIp, string expected)
+        {
+            string ip = converter.Convert(intIp);
+            ip.Should().Be(expected);
+        }
+    }
+}
---
+++ Kata/CodeWar/IpConverter.cs
@@ -0,0 +1,16 @@
+﻿namespace Kata.CodeWar
+{
+    public class IpConverter
+    {
+        public string Convert(uint intIp)
+        {
+            string[] ipPart = new string[4];
+            for (int partIndex = 3; partIndex >= 0; partIndex--)
+            {
+                ipPart[partIndex] = System.Convert.ToString(intIp % 256U);
+                intIp /= 256U;
+            }
+            return string.Join(".", ipPart);
+        }
+    }
+}
