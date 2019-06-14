b93dc477e55b2efbfe757fa26ca639ea9726bf43
hendryboyz <hendry.js1247@gmail.com>
2019-06-14 09:47:42 +0800

    Codewar Kata : Connect Four - some defect

---
+++ Kata.Tests/ConnectFourTests.cs
@@ -0,0 +1,131 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+// https://www.codewars.com/kata/connect-four-1/train/csharp
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class ConnectFourTests
+    {
+        private ConnectFour connectFour;
+
+        [Test]
+        public void CanCreate()
+        {
+            connectFour = new ConnectFour();
+            connectFour.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void CanWhoIsWinner()
+        {
+            string winner = connectFour.WhoIsWinner(new List<string>());
+            winner.Should().Be("Draw");
+        }
+        [Test]
+        public void GivenRedWin_WhenWhoIsWinner_ThenReturnRed()
+        {
+            List<string> boardStatus = new List<string>()
+            {
+                "A_Red",
+                "B_Yellow",
+                "A_Red",
+                "B_Yellow",
+                "A_Red",
+                "B_Yellow",
+                "A_Red"
+            };
+            string winner = connectFour.WhoIsWinner(boardStatus);
+            winner.Should().Be("Red");
+        }
+
+        [Test]
+        public void GivenYellowWin_WhenWhoIsWinner_ThenReturnYellow()
+        {
+            List<string> boardStatus = new List<string>()
+            {
+                "A_Red",
+                "B_Yellow",
+                "A_Red",
+                "B_Yellow",
+                "A_Red",
+                "B_Yellow",
+                "G_Red",
+                "B_Yellow"
+            };
+            string winner = connectFour.WhoIsWinner(boardStatus);
+            winner.Should().Be("Yellow");
+        }
+
+        [Test]
+        public void GivenYellowWin2_WhenWhoIsWinner_ThenReturnYellow()
+        {
+            List<string> boardStatus = new List<string>()
+            {
+                "C_Yellow",
+                "E_Red",
+                "G_Yellow",
+                "B_Red",
+                "D_Yellow",
+                "B_Red",
+                "B_Yellow",
+                "G_Red",
+                "C_Yellow",
+                "C_Red",
+                "D_Yellow",
+                "F_Red",
+                "E_Yellow",
+                "A_Red",
+                "A_Yellow",
+                "G_Red",
+                "A_Yellow",
+                "F_Red",
+                "F_Yellow",
+                "D_Red",
+                "B_Yellow",
+                "E_Red",
+                "D_Yellow",
+                "A_Red",
+                "G_Yellow",
+                "D_Red",
+                "D_Yellow",
+                "C_Red"
+            };
+            string winner = connectFour.WhoIsWinner(boardStatus);
+            winner.Should().Be("Yellow");
+        }
+
+        [Test]
+        public void GivenRedwWin2_WhenWhoIsWinner_ThenReturnRed()
+        {
+            List<string> boardStatus = new List<string>()
+            {
+               "A_Yellow",
+                "B_Red",
+                "B_Yellow",
+                "C_Red",
+                "G_Yellow",
+                "C_Red",
+                "C_Yellow",
+                "D_Red",
+                "G_Yellow",
+                "D_Red",
+                "G_Yellow",
+                "D_Red",
+                "F_Yellow",
+                "E_Red",
+                "D_Yellow"
+            };
+            string winner = connectFour.WhoIsWinner(boardStatus);
+            winner.Should().Be("Red");
+        }
+    }
+}
---
+++ Kata/CodeWar/ConnectFour.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+
+namespace Kata.CodeWar
+{
+    public class ConnectFour
+    {
+        private readonly string DRAW = "Draw";
+        private readonly string RED = "Red";
+        private readonly string YELLOW = "Yellow";
+
+        public string WhoIsWinner(List<string> boardState)
+        {
+            int[,] board = new int[6, 7];
+            int[] columnIndex = new int[7];
+            foreach (string eachPlay in boardState)
+            {
+                string[] playInfo = eachPlay.Split("_");
+                int column = playInfo[0][0] - 'A';
+                string color = playInfo[1];
+                board[columnIndex[column]++, column] = color.Equals(RED) ? 1 : 2;
+            }
+
+            int[,] scores = new int[6, 7];
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    int color = board[i, j];
+                    if (0 == color) continue;
+                    int maxConnect = GetPreviousMaxConnection(board, scores, i, j, color);
+                    scores[i, j] = maxConnect + 1;
+                    if (4 == scores[i, j])
+                    {
+                        return board[i, j] == 1 ? RED : YELLOW;
+                    }
+                }
+            }
+            return DRAW;
+        }
+
+        private int GetPreviousMaxConnection(int[,] board, int[,] scores, int i, int j, int color)
+        {
+            int maxConnect = 0;
+            int previousConnect = GetConnectCount(i - 1, j - 1, board, scores);
+            if (previousConnect > 0 &&
+                previousConnect > maxConnect &&
+                color == board[i - 1, j - 1])
+            {
+                maxConnect = scores[i - 1, j - 1];
+            }
+
+            previousConnect = GetConnectCount(i - 1, j, board, scores);
+            if (previousConnect > 0 &&
+                previousConnect > maxConnect &&
+                color == board[i - 1, j])
+            {
+                maxConnect = scores[i - 1, j];
+            }
+
+            previousConnect = GetConnectCount(i, j - 1, board, scores);
+            if (previousConnect > 0 &&
+                previousConnect > maxConnect &&
+                color == board[i, j - 1])
+            {
+                maxConnect = scores[i, j - 1];
+            }
+
+            previousConnect = GetConnectCount(i - 1, j + 1, board, scores);
+            if (previousConnect > 0 &&
+                previousConnect > maxConnect &&
+                color == board[i - 1, j + 1])
+            {
+                maxConnect = scores[i - 1, j + 1];
+            }
+
+            return maxConnect;
+        }
+
+        private int GetConnectCount(int row, int col, int[,] board, int[,] scores)
+        {
+            if (row < 0 || 6 <= row ||
+                col < 0 || 7 <= col )
+            {
+                return 0;
+            }
+            return scores[row, col];
+        }
+    }
+}
