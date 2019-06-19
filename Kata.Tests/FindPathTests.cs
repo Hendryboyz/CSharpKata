116027d8ed3ac70bd6e4363575e5d08e981f0eb8
hendryboyz <hendry.js1247@gmail.com>
2019-06-19 08:58:03 +0800

    Codewar Kata : Path Finder #1: can you reach the exit?
    Not finishing

---
+++ Kata.Tests/FindPathTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class FindPathTests
+    {
+        private FindPath findPath;
+
+        [Test]
+        public void CanCreate()
+        {
+            findPath = new FindPath();
+            findPath.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void TestIsExitExisting()
+        {
+            string maze = "..\n" +
+                          ".W";
+            bool isExitExisting = findPath.IsExitExisting(maze);
+            isExitExisting.Should().BeFalse();
+        }
+
+        [Test]
+        public void Given4By4MazeWithExit_WhenIsExitExisting_ThenReturnTrue()
+        {
+            string maze = ".W\n" +
+                          "..";
+            bool isExitExisting = findPath.IsExitExisting(maze);
+            isExitExisting.Should().BeTrue();
+        }
+    }
+}
---
+++ Kata/CodeWar/FindPath.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Kata.CodeWar
+{
+    public class FindPath
+    {
+        private readonly char WALL = 'W';
+        private readonly char ROAD = '.';
+
+        public bool IsExitExisting(string maze)
+        {
+            char[,] mazeArray = ConvertToMazeArray(maze);
+            return IsPathExisting(mazeArray, 0, 0);
+        }
+
+        private bool IsPathExisting(char[,] mazeArray, int row, int column)
+        {
+            int sideLength = mazeArray.GetLength(0);
+            if ((row < 0 || sideLength <= row) ||
+                (column < 0 || sideLength <= column))
+            {
+                return false;
+            }
+            else if (mazeArray[row, column] == WALL)
+            {
+                return false;
+            }
+            else if (row == sideLength - 1 && column == sideLength - 1)
+            {
+                return true;
+            }
+            else
+            {
+                if (IsPathExisting(mazeArray, row - 1, column))
+                {
+                    return true;
+                }
+                else if(IsPathExisting(mazeArray, row + 1, column))
+                {
+                    return true;
+                }
+                else if (IsPathExisting(mazeArray, row, column - 1))
+                {
+                    return true;
+                }
+                else if (IsPathExisting(mazeArray, row, column + 1))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        private char[,] ConvertToMazeArray(string maze)
+        {
+            string[] rowInfo = maze.Split("\n");
+            int rowCount = rowInfo.Length;
+            int colCount = rowInfo[0].Length;
+            char[,] mazeInfo = new char[rowCount, colCount];
+            for (int rowIdx = 0; rowIdx < rowCount; rowIdx++)
+            {
+                for (int colIdx = 0; colIdx < colCount; colIdx++)
+                {
+                    mazeInfo[rowIdx, colIdx] = rowInfo[rowIdx][colIdx];
+                }
+            }
+            return mazeInfo;
+        }
+    }
+}
