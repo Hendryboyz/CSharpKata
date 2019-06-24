22d21d2b0823a3e68990028217f759c19ca48937
hendryboyz <hendry.js1247@gmail.com>
2019-06-24 09:48:25 +0800

    Codewar kata : twicelinear - not complete

---
+++ Kata.Tests/TwicelinearTests.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Kata.CodeWar;
+using NUnit.Framework;
+using System;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class TwiceLinearTests
+    {
+        private TwiceLinear algorithm;
+
+        [Test]
+        public void CanCreate()
+        {
+            algorithm = new TwiceLinear();
+            algorithm.Should().NotBeNull();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CanCreate();
+        }
+
+        [Test]
+        public void CanGetAt()
+        {
+            int result = algorithm.GetAt(0);
+            result.Should().Be(1);
+        }
+
+        [TestCase(2, 4)]
+        [TestCase(10, 22)]
+        public void GivenSimpleCase_WhenGetAt_ThenReturnIntAtPositionN(int n, int numberAtN)
+        {
+            int result = algorithm.GetAt(n);
+            result.Should().Be(numberAtN);
+        }
+
+        [TestCase(20, 57)]
+        [TestCase(30, 91)]
+        [TestCase(50, 175)]
+        public void GivenMediumSizeCaseWhenGetAt_ThenReturnIntAtPositionN(int n, int numberAtN)
+        {
+            int result = algorithm.GetAt(n);
+            result.Should().Be(numberAtN);
+        }
+
+        [Test]
+        [Timeout(12000)]
+        public void GivenLargeSizeCaseWhenGetAt_ThenReturnIntNotTimeout()
+        {
+            int result = algorithm.GetAt(60000);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Kata/CodeWar/TwiceLinear.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Kata.CodeWar
+{
+    public class TwiceLinear
+    {
+        public int GetAt(int n)
+        {
+            
+            IList<int> linearList = new List<int>();
+            Queue<int> yQueue = InitialQueue();
+            Queue<int> zQueue = InitialQueue();
+            while (linearList.Count <= n)
+            {
+                int current = 1;
+                if (yQueue.Peek() < zQueue.Peek())
+                {
+                    current = yQueue.Dequeue();
+                }
+                else
+                {
+                    current = zQueue.Dequeue();
+                }
+                yQueue.Enqueue(current * 2 + 1);
+                zQueue.Enqueue(current * 3 + 1);
+                if (!linearList.Contains(current))
+                {
+                    linearList.Add(current);
+                }
+            }
+            return linearList[n];
+        }
+
+        private Queue<int> InitialQueue()
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            return queue;
+        }
+    }
+}
